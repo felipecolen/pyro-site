c01effa5b73924e28a287820be55b65f837e9333
Felipe Oliveira Colen <felipecolen@gmail.com>
2018-05-12 15:08:02 -0400

    add template base

--- web/controllers.py
+++ web/controllers.py
@@ -1,9 +1,13 @@
-from flask import Blueprint
-
+from flask import Blueprint, render_template
 
 api = Blueprint('api', __name__)
 
 
 @api.route("/")
 def index():
-    return "<h3>hello flask</h3>"
+    return "<h1>hello flask</h1>"
+
+
+@api.route("/html")
+def index_html():
+    return render_template('index.html')
