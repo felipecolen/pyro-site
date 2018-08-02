694e20b09af5d2190a73918f78e5581562ca1cb7
Felipe Oliveira Colen <felipecolen@gmail.com>
2018-08-01 22:02:36 -0400

    refatorando codigo para ficar mais simples

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
 import os
 
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, render_template
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
 
-from web import create_app
-app = create_app()
+
+@app.route("/teste")
+def index():
+    return "<h1>hello flask</h1>"
+
+
+@app.route("/")
+def index_html():
+
+    # form = PostForm()
+    # if form.validate_on_submit():
+    #     language = guess_language(form.post.data)
+    #     if language == 'UNKNOWN' or len(language) > 5:
+    #         language = ''
+    #     post = Post(body=form.post.data, author=current_user,
+    #                 language=language)
+    #     db.session.add(post)
+    #     db.session.commit()
+    #     flash(_('Your post is now live!'))
+    #     return redirect(url_for('main.index'))
+    # page = request.args.get('page', 1, type=int)
+    # posts = current_user.followed_posts().paginate(
+    #     page, current_app.config['POSTS_PER_PAGE'], False)
+    # next_url = url_for('main.index', page=posts.next_num) \
+    #     if posts.has_next else None
+    # prev_url = url_for('main.index', page=posts.prev_num) \
+    #     if posts.has_prev else None
+
+    # noticias = Noticia.query.order_by(Noticia.data_adicao.desc())
+
+    return render_template('index.html',
+                           # title=_('Home'),
+                           # form=form,
+                           # noticias=noticias
+                           )
 
 
 if __name__ == '__main__':
